4f51c197fca1442fc869d67d402d4773b2accd79
ernest_aitiev <valeetmihifaveas@gmail.com>
2019-12-01 20:24:23 +0600

    Добавлена проверка на то, что отправлены действительно числа

--- Homework68/source/webapp/views.py
+++ Homework68/source/webapp/views.py
@@ -10,37 +10,55 @@ def add_numbers(request, *args, **kwargs):
     data = None
     if request.body:
         data = json.loads(request.body)
-    answer = {
-            'answer': data['A'] + data['B'],
-    }
-    return JsonResponse(answer)
-
+    if check_if_numbers(data):
+        answer = {
+                'answer': data['A'] + data['B'],
+        }
+        return JsonResponse(answer)
+    else:
+        return JsonResponse({'error': 'One value is not a number!'}, status=400)
+    
 @csrf_exempt
 def substract_numbers(request, *args, **kwargs):
     data= None
     if request.body:
         data = json.loads(request.body)
-    answer = {
-        'answer': data['A'] - data['B'],
-    }
-    return JsonResponse(answer)
+    if check_if_numbers(data):
+        answer = {
+            'answer': data['A'] - data['B'],
+        }
+        return JsonResponse(answer)
+    else:
+        return JsonResponse({'error': 'One value is not a number!'}, status=400)
 
 @csrf_exempt
 def multiply_numbers(request, *args, **kwargs):
     data= None
     if request.body:
         data = json.loads(request.body)
-    answer = {
-        'answer': data['A'] * data['B'],
-    }
-    return JsonResponse(answer)
+    if check_if_numbers(data):
+        answer = {
+            'answer': data['A'] * data['B'],
+        }
+        return JsonResponse(answer)
+    else:
+        return JsonResponse({'error': 'One value is not a number!'}, status=400)
+
 
 @csrf_exempt
 def divide_numbers(request, *args, **kwargs):
     data= None
     if request.body:
         data = json.loads(request.body)
-    answer = {
-        'answer': data['A'] / data['B'],
-    }
-    return JsonResponse(answer)
+    if check_if_numbers(data):
+        answer = {
+            'answer': data['A'] / data['B'],
+        }
+        return JsonResponse(answer)
+    else:
+        return JsonResponse({'error': 'One value is not a number!'}, status=400)
+
+
+def check_if_numbers(data):
+    if not isinstance(data['A'], int) or not isinstance(data['B'], int):
+        return False
